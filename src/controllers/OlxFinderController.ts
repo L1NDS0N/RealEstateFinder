ac869f930372ac7a20d289c006fa9581d588a019
Lindson França <l1nds0n@hotmail.com>
2021-04-13 17:50:30 -0300

    Syntax works fine, but the function in meth is not working well
    prisma.pesquisa.update is not working when parsing href from loop to the new Link object in table

--- src/controllers/OlxFinderController.ts
+++ src/controllers/OlxFinderController.ts
@@ -3,10 +3,10 @@ import { Request, Response } from 'express';
 import { Sweeping } from '../services/SweepScrap';
 
 const prisma = new PrismaClient();
-
 class OlxFinderController {
   async NovaPesquisa(req: Request, res: Response) {
     const { url, cidade, tipo } = req.body;
+
     const pesquisaCriada = await prisma.pesquisa.create({
       data: {
         url,
@@ -14,9 +14,22 @@ class OlxFinderController {
         tipo,
       },
     });
-    await Sweeping(pesquisaCriada).then(urls => {});
 
+    await Sweeping(pesquisaCriada).then(hrefs => {
+      hrefs.map(href => {
+        console.log('this is the href updatind now:', href);
+        prisma.pesquisa.update({
+          where: { id: pesquisaCriada.id },
+          data: {
+            Links: {
+              create: { href: href },
+            },
+          },
+        });
+      });
+    });
     await prisma.$disconnect();
+    return res.json(pesquisaCriada);
   }
 }
 export { OlxFinderController };
