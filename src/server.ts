6acd066ece76cebddef9143202fb1de783bc7bf2
Lindson França <l1nds0n@hotmail.com>
2021-04-14 13:26:33 -0300

    refactoring directories

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,4 @@
-import express from 'express';
-import { OlxFinderController } from './controllers/OlxFinderController';
-const app = express();
-import { router } from './routes';
-
-const olxFinder = new OlxFinderController();
-
-app.use(express.json());
-
-app.use(router);
+import { app } from './app';
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
