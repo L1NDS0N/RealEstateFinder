8154b09da7b73c672cb2d677628809f7169965b5
Lindson França <l1nds0n@hotmail.com>
2021-04-14 13:27:02 -0300

    Validation on req submit with yup

--- src/controllers/OlxFinderController.ts
+++ src/controllers/OlxFinderController.ts
@@ -1,11 +1,29 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
 import { Sweeping } from '../services/SweepScrap';
+import * as yup from 'yup';
+import { pt } from 'yup-locale-pt';
+import { AppError } from '../errors/AppError';
+
+yup.setLocale(pt);
 
 const prisma = new PrismaClient();
 class OlxFinderController {
   async NovaPesquisa(req: Request, res: Response) {
     const { url, cidade, tipo } = req.body;
+    const schema = yup.object().shape({
+      url: yup.string().url().required(),
+      cidade: yup.string().notRequired(),
+      tipo: yup.string().notRequired(),
+    });
+
+    try {
+      await schema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+      throw new AppError(err);
+    }
+
+    res.status(400).json({ error: 'URL cannot be null' });
 
     const pesquisaCriada = await prisma.pesquisa.create({
       data: {
@@ -17,19 +35,21 @@ class OlxFinderController {
 
     await Sweeping(pesquisaCriada)
       .then(hrefs => {
-          hrefs.map(async href => {
-            await prisma.pesquisa.update({
+        hrefs.map(async href => {
+          await prisma.pesquisa
+            .update({
               where: { id: pesquisaCriada.id },
               data: {
                 Links: {
                   create: { href },
                 },
               },
-            }).catch(e => {
+            })
+            .catch(e => {
               console.log(e);
             });
-          });
-        })
+        });
+      })
       .catch(e => {
         console.log(e);
       });
@@ -38,4 +58,5 @@ class OlxFinderController {
     return res.json(pesquisaCriada);
   }
 }
+
 export { OlxFinderController };
